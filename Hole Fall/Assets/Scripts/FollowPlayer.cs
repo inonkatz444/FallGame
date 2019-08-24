a25bb2287741d9ee15eaa150928ad28d1b6faaaf
inon katz <inonkatz444@gmail.com>
2019-08-24 19:03:01 +0300

    first commit

---
+++ Hole Fall/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Transform upperWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = new Vector3(0, 2 / 3f, -2.5f);
+        transform.position = player.position + offset;
+    }
+}
---
+++ Hole Fall/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody player;
+    public float movementForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("a"))
+        {
+            player.AddForce(-movementForce, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            player.AddForce(movementForce, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("w"))
+        {
+            player.AddForce(0, movementForce, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            player.AddForce(0, -movementForce, 0, ForceMode.VelocityChange);
+        }
+    }
+}
